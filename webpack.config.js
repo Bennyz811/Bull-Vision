13a650446b28ca0adc5042bc22c66fa8fe3184d1
Benny Zhao <bennyz811@gmail.com>
2018-04-03 18:40:57 -0400

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,10 @@ const env = process.env.NODE_ENV;
 
 module.exports = {
   entry: "./src/bull.js",
-  mode: env || 'development',
+  mode: env || "development",
   output: {
     filename: "./bundle.js"
   },
-    module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
-     ]
- },
   devtool: "source-map",
   resolve: {
     extensions: [".js", "*"]
