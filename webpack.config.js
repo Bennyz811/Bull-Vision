2b58541126874d8f955ae711fa535bb35166731d
Benny Zhao <bennyz811@gmail.com>
2018-04-03 15:18:05 -0400

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+const env = process.env.NODE_ENV;
+
+module.exports = {
+  entry: "./src/bull.js",
+  mode: env || 'development',
+  output: {
+    filename: "./bundle.js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", "*"]
+  }
+};
