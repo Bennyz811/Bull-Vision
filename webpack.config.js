b523b35d5793df9eafb95ac364001a4cff225073
Benny Zhao <bennyz811@gmail.com>
2018-04-04 16:07:45 -0400

    added async/await packages and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,32 @@
 var path = require("path");
 
 const env = process.env.NODE_ENV;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/bull.js",
+  entry: ["babel-polyfill", "./src/bull.js"],
   mode: env || "development",
   output: {
     filename: "./bundle.js"
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: ['transform-async-to-generator'],
+            presets: ['babel-preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
   devtool: "source-map",
   resolve: {
     extensions: [".js", "*"]
